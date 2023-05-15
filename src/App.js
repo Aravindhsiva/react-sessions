5d455c68d8c3bdc9734fa51ac934b14785226d15
Aravindhsiva <adamaravindh@gmail.com>
2023-05-15 15:18:37 +0530

    Adding Reactor Router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { Fragment, useState } from 'react';
+import React, { Suspense } from 'react';
+import DashboardScreen from './screens/router/DashboardScreen';
+import { Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/router/HomeScreen';
+import FourNotFourScreen from './screens/router/FourNotFourScreen';
+// import CartScreen from './screens/router/CartScreen';
 
-// import LifeCycleComp from './screens/lifeCycle/lifecycle';
-// import ProductScreen from './screens/lifeCycle/response-transform';
-import PropsExample from './screens/props/props';
-import BodyWrapper from './screens/props/body';
-import HomeScreen from './screens/props/homeScreen';
-import DashBoard from './screens/props/dashboardScreen';
+import Product from './screens/router/Product';
+import Order from './screens/router/Order';
 
-const App =  () =>{
-  const [toggle, setToggle] = useState(false);
-  return <Fragment>
-    <BodyWrapper >
-      {toggle ? <DashBoard /> : <HomeScreen />}
-    </BodyWrapper>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <button className='btn btn-primary' onClick={e => { setToggle(!toggle) }}>Toggle</button>
-  </Fragment>
+const CartScreen = React.lazy(() => import("./screens/router/CartScreen"));
+const App = () => {
+  return <Routes>
+    <Route path='/' element={<HomeScreen />} ></Route>
+    <Route path='dashboard' element={<DashboardScreen />} ></Route>
+    <Route path="cart" element={
+    <Suspense>
+      <CartScreen />
+    </Suspense>} >
+      <Route index element={<Product />} />
+      <Route path='product' element={<Product />} ></Route>
+      <Route path='order' element={<Order />} ></Route>
+    </Route>
+    <Route path="*" element={<FourNotFourScreen />} />
+  </Routes>
 }
+
+/**
+ * / -> home
+ * /dashboard -> Dashboard
+ * /cart -> CartScreens
+ *
+ */
 
 export default App;
---
+++ src/screens/router/FourNotFourScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FourNotFourScreen = () => {
+  return (
+    <div>FourNotFourScreen</div>
+  )
+}
+
+export default FourNotFourScreen
